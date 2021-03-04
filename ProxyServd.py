a2bd6aa7f632e61ef49aceec5fce18ec17647793
kaimei <freekai@outlook.com>
2021-03-04 19:02:29 +0800

    ...

--- ProxyServd.py
+++ ProxyServd.py
@@ -1,2 +1,80 @@
 #!/usr/bin/env python3
 
+import sys, os, json, getopt, socket, signal, time
+
+BASE_DIR = os.path.dirname(sys.argv[0])
+
+if not BASE_DIR: BASE_DIR = "."
+
+sys.path.append(BASE_DIR)
+
+import libProxyServd.proc as proc
+import libProxyServd.handlers.proxyd as proxy
+
+PID_PATH = "/tmp/ixc_ProxyServd.pid"
+
+
+class proxyd(object):
+    def __init__(self, debug=False):
+        self.__debug = debug
+
+    def load_configs(self):
+        pass
+
+    def release(self):
+        pass
+
+
+def stop():
+    pid = proc.get_pid(PID_PATH)
+    if pid < 0: return
+    os.remove(PID_PATH)
+    os.kill(pid, signal.SIGINT)
+
+
+def main():
+    help_doc = """
+    debug | start | stop
+    """
+
+    if len(sys.argv) < 2:
+        print(help_doc)
+        return
+
+    action = sys.argv[1]
+
+    if action not in ("debug", "stop", "start",):
+        print(help_doc)
+        return
+
+    if action == "stop":
+        stop()
+        return
+
+    try:
+        opts, args = getopt.getopt(sys.argv[2:], "", ["port=", "bind_ip="])
+    except getopt.GetoptError:
+        print(help_doc)
+        return
+
+    debug = True
+    if action == "start":
+        debug = False
+        pid = os.fork()
+        if pid != 0: sys.exit(0)
+
+        os.setsid()
+        os.umask(0)
+
+        pid = os.fork()
+        if pid != 0: sys.exit(0)
+        proc.write_pid(PID_PATH)
+
+    cls = proxyd(debug=debug)
+    try:
+        cls.monitor()
+    except KeyboardInterrupt:
+        cls.release()
+
+
+if __name__ == '__main__': main()
--- libProxyServd/handlers/proxyd.py
+++ libProxyServd/handlers/proxyd.py
@@ -1,2 +1,180 @@
 #!/usr/bin/env python3
 
+import socket, time, hashlib
+
+import pywind.evtframework.handlers.tcp_handler as tcp_handler
+import pywind.web.lib.httputils as httputils
+import pywind.web.lib.websocket as wslib
+
+import libProxyServd.protocol as protocol
+
+
+class listener(tcp_handler.tcp_handler):
+    def init_func(self, creator, address, is_ipv6=False):
+        if is_ipv6:
+            fa = socket.AF_INET6
+        else:
+            fa = socket.AF_INET
+
+        s = socket.socket(fa, socket.SOCK_STREAM)
+        if is_ipv6: s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 1)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        self.set_socket(s)
+        self.bind(address)
+        self.listen(10)
+        self.register(self.fileno)
+        self.add_evt_read(self.fileno)
+
+        return self.fileno
+
+    def tcp_accept(self):
+        while 1:
+            try:
+                cs, address = self.accept()
+                self.create_handler(self.fileno, cs, address)
+            except BlockingIOError:
+                break
+            ''''''
+        return
+
+    def tcp_delete(self):
+        self.unregister(self.fileno)
+        self.close()
+
+
+class proxy_handler(tcp_handler.tcp_handler):
+    __address = None
+
+    __update_time = 0
+    __conn_timeout = 0
+
+    __LOOP_TIMEOUT = 5
+
+    __session_id = None
+
+    __over_http = None
+    __http_handshake_ok = None
+    __http_auth_id = None
+    __http_ws_key = None
+
+    def init_func(self, creator, cs, caddr):
+        self.__address = caddr
+        self.__update_time = time.time()
+        self.__session_id = None
+
+        self.__http_handshake_ok = False
+        cs.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        self.set_socket(cs)
+        self.set_timeout(self.fileno, self.__LOOP_TIMEOUT)
+
+        self.register(self.fileno)
+        self.add_evt_read(self.fileno)
+
+        return self.fileno
+
+    def tcp_readable(self):
+        if not self.__http_handshake_ok:
+            self.do_http_handshake()
+            return
+        rdata = self.reader.read()
+
+    def tcp_writable(self):
+        if self.writer.size() == 0: self.remove_evt_write(self.fileno)
+
+    def tcp_error(self):
+        self.delete_handler(self.fileno)
+
+    def tcp_timeout(self):
+        t = time.time()
+
+        if t - self.__update_time > self.__conn_timeout:
+            self.delete_handler(self.fileno)
+            return
+        self.set_timeout(self.fileno, self.__LOOP_TIMEOUT)
+
+    def tcp_delete(self):
+        self.unregister(self.fileno)
+        self.close()
+
+    def send_msg(self, session_id, address, action, message):
+        # 检查session_id是否一致
+        if session_id != self.__session_id: return
+        if not self.__http_handshake_ok: return
+        self.add_evt_write(self.fileno)
+
+    def do_http_handshake(self):
+        size = self.reader.size()
+        data = self.reader.read()
+
+        p = data.find(b"\r\n\r\n")
+
+        if p < 10 and size > 2048:
+            self.delete_handler(self.fileno)
+            return
+
+        if p < 0:
+            self.reader._putvalue(data)
+            return
+        p += 4
+
+        self.reader._putvalue(data[p:])
+
+        s = data[0:p].decode("iso-8859-1")
+
+        try:
+            request, kv_pairs = httputils.parse_htt1x_request_header(s)
+        except httputils.Http1xHeaderErr:
+            self.delete_handler(self.fileno)
+            return
+
+        method, url, version = request
+        upgrade = self.get_http_kv_value("upgrade", kv_pairs)
+        origin = self.get_http_kv_value("origin", kv_pairs)
+
+        if upgrade != "websocket" and method != "GET":
+            self.response_http_error("400 Bad Request")
+            return
+
+        if not origin:
+            self.response_http_error("403 Forbidden")
+            return
+
+        self.__http_ws_key = self.get_http_kv_value("sec-websocket-key", kv_pairs)
+        if not self.__http_ws_key:
+            self.response_http_error("400 Bad Request")
+            return
+
+        self.__http_handshake_ok = True
+        self.response_http_ok()
+
+    def response_http(self, status):
+        headers = [("Content-Length", 0,)]
+
+        if status[0:3] == "101":
+            headers += [("Connection", "Upgrade",), ("Upgrade", "websocket",)]
+            headers += [("Sec-WebSocket-Accept", wslib.gen_handshake_key(self.__http_ws_key))]
+            headers += [("Sec-WebSocket-Protocol", "chat")]
+            headers += [("X-Auth-Id", hashlib.sha256(self.__http_auth_id.encode()).hexdigest())]
+        s = httputils.build_http1x_resp_header(status, headers)
+
+        self.add_evt_write(self.fileno)
+        self.writer.write(s.encode("iso-8859-1"))
+
+    def response_http_error(self, status):
+        self.response_http(status)
+        self.delete_this_no_sent_data()
+
+    def response_http_ok(self):
+        self.response_http("101 Switching Protocols")
+
+    def get_http_kv_value(self, name, kv_pairs):
+        for k, v in kv_pairs:
+            if name.lower() == k.lower():
+                return v
+            ''''''
+        return None
+
+    @property
+    def session_id(self):
+        return self.__session_id
