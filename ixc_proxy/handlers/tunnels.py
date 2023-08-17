6c90c8f03dc77d5cb82a9ee5b4448e01d55a92a4
freek <freekai@outlook.com>
2023-08-17 23:15:05 +0800

    ...

--- ixc_proxy/handlers/tunnels.py
+++ ixc_proxy/handlers/tunnels.py
@@ -292,6 +292,7 @@ class udp_tunnel(udp_handler.udp_handler):
 
     def udp_readable(self, message, address):
         result = self.__decrypt.parse(message)
+        print(result,address)
         if not result: return
 
         session_id, action, byte_data = result
