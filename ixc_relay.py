6ee75b70062f0186ab5291cd63d3d08066e11b44
freek <freekai@outlook.com>
2023-11-25 16:52:03 +0800

    ...

--- ixc_relay.py
+++ ixc_relay.py
@@ -125,7 +125,7 @@ def main():
     try:
         opts, args = getopt.getopt(sys.argv[1:], "6p:",
                                    ["nofork", "bind=", "redirect=", "help", "limit-month-traffic=",
-                                    "udp-heartbeat-file="])
+                                    "udp-heartbeat-file=","tcp-redirect-slave="])
     except getopt.GetoptError:
         print(help_doc)
         return
